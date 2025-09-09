7fb6ece8752d109615d96a24ab4a427e8ea16bf8
manar27112000 <adel0115225@gmail.com>
2025-09-09 16:59:21 +0300

    add task method to add task cubit

--- lib/features/tasks_screen/view_model/add_task/add_task_cubit.dart
+++ lib/features/tasks_screen/view_model/add_task/add_task_cubit.dart
@@ -1,13 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 
 import '../../model/task_model.dart';
 import 'add_task_state.dart';
-
 
 class AddTaskCubit extends Cubit<AddTaskState> {
   AddTaskCubit() : super(AddTaskInitialState());
 
   addTask(TaskModel taskModel) {
-
+   emit(AddTaskLoadingState());
+   try {
+    var taskBox = Hive.box<TaskModel>('tasks');
+    emit(AddTaskSuccessState());
+    taskBox.add(taskModel);
+   } catch (e) {
+    emit(AddTaskFailureState(e.toString()));
+   }
   }
 }
