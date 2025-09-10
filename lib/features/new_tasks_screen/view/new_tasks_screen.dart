8bb260a4c8e19d38fda5f44e5c38bc0b2e61adaa
manar27112000 <adel0115225@gmail.com>
2025-09-10 23:01:23 +0300

    refactor multi bloc provider

--- lib/features/new_tasks_screen/view/new_tasks_screen.dart
+++ lib/features/new_tasks_screen/view/new_tasks_screen.dart
@@ -9,33 +9,32 @@ class NewTasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => FocusScope.of(context).unfocus(),
-        child:  SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 36),
-            child: BlocConsumer<AddTaskCubit, AddTaskState>(
-              listener: (context, state) {
-                if(state is AddTaskSuccessState){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Task added successfully')),
-                  );
-                }
-                if(state is AddTaskFailureState){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Failed to add task, try again')),
-                  );
-
-                }
-
-
-
-              },
-              builder: (context, state) {
-                return AddTaskForm();
-              },
+    return BlocProvider(
+      create: (context) => AddTaskCubit(),
+      child: Scaffold(
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 36),
+              child: BlocConsumer<AddTaskCubit, AddTaskState>(
+                listener: (context, state) {
+                  if (state is AddTaskSuccessState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Task added successfully')),
+                    );
+                  }
+                  if (state is AddTaskFailureState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Failed to add task, try again')),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return AddTaskForm();
+                },
+              ),
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddTaskCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Todo List',
-      home: LayoutScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List',
+    home: LayoutScreen(),
 
 
 
 
-      ),
     );
   }
 }
