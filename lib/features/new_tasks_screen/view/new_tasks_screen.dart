9b17293b49458bc85b60f5878922725eed32de49
manar27112000 <adel0115225@gmail.com>
2025-09-10 20:37:02 +0300

    add add task cubit to add task screen

--- lib/features/new_tasks_screen/view/new_tasks_screen.dart
+++ lib/features/new_tasks_screen/view/new_tasks_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_list1/features/new_tasks_screen/view/widgets/add_task_form.dart';
+import 'package:todo_list1/features/tasks_screen/view_model/add_task/add_task_cubit.dart';
+import 'package:todo_list1/features/tasks_screen/view_model/add_task/add_task_state.dart';
 
 class NewTasksScreen extends StatelessWidget {
   const NewTasksScreen({super.key});
@@ -10,10 +13,30 @@ class NewTasksScreen extends StatelessWidget {
       body: GestureDetector(
         behavior: HitTestBehavior.opaque,
         onTap: () => FocusScope.of(context).unfocus(),
-        child: const SafeArea(
+        child:  SafeArea(
           child: Padding(
-            padding:  EdgeInsets.symmetric(vertical: 8.0, horizontal: 36),
-            child: AddTaskForm(),
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 36),
+            child: BlocConsumer<AddTaskCubit, AddTaskState>(
+              listener: (context, state) {
+                if(state is AddTaskSuccessState){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Task added successfully')),
+                  );
+                }
+                if(state is AddTaskFailureState){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to add task, try again')),
+                  );
+
+                }
+
+
+
+              },
+              builder: (context, state) {
+                return AddTaskForm();
+              },
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:todo_list1/features/layout_screen/view/layout_screen.dart';
+import 'package:todo_list1/features/tasks_screen/view_model/add_task/add_task_cubit.dart';
 
 import 'features/tasks_screen/model/task_model.dart';
 
@@ -18,14 +20,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Todo List',
-home: LayoutScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddTaskCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Todo List',
+      home: LayoutScreen(),
 
 
 
 
+      ),
     );
   }
 }
